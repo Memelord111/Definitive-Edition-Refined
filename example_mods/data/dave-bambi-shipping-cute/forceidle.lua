165994c1b3315d3e1c81bd4016ea1baef2dfdba9
Memelord111 <DakCam123@hotmail.com>
2024-10-01 10:07:18 -0400

    e

--- example_mods/data/dave-bambi-shipping-cute/forceidle.lua
+++
@@ -1,8 +0,0 @@
-function onUpdate()
-	if stringStartsWith(getProperty'boyfriend.animation.curAnim.name', 'sing') and getProperty 'boyfriend.animation.curAnim.finished' then
-		runHaxeCode 'game.boyfriend.dance();'
-	end
-	if stringStartsWith(getProperty'dad.animation.curAnim.name', 'sing') and getProperty 'dad.animation.curAnim.finished' then
-		runHaxeCode 'game.dad.dance();'
-	end
-end
--- example_mods/data/polygonized/script 5.lua
+++
@@ -1,6 +0,0 @@
-function opponentNoteHit()
-	health = getProperty('health')
-	if health > 0.02 then
-		setProperty('health', health - 0.003);	
-	end
-end
